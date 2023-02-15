b56dd92021e8dde0ece0ac119c0fc01a2570a463
ahmad <ahmadali009ali008@gmail.com>
2023-02-15 17:00:03 +0000

    freqtrade front end one

---
+++ src/plugins/bootstrap-vue.ts
@@ -0,0 +1,7 @@
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+import '@/styles/main.scss';
+
+export { BootstrapVue3 };
